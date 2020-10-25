dcaea24515298033183c5e69616c5577dadf8e1c
mfchn <69769377+mfchn@users.noreply.github.com>
2020-10-25 11:27:24 -0500

    Add files via upload

---
+++ final2/pru1.sh
@@ -0,0 +1,18 @@
+#¡/bin/bash
+echo "hola mundo"
+	flex lexer.l
+	bison -d parser.y
+	gcc -c lex.yy.c parser.tab.c
+	gcc -o lexpar lex.yy.o parser.tab.o -ll
+read -p "Ingrese nombre archivo: " NOMBREARCH
+MENSAJE=`./lexpar < $NOMBREARCH.txt`
+VAR=''
+if [[ -z $MENSAJE ]];then
+	mono traductor.exe $NOMBREARCH
+	javac salida.java
+	echo "--------------------------------------"
+	java ClaseTraducida
+else
+	echo $MENSAJE
+fi
+exit
---
+++ final2/salida.java
@@ -0,0 +1,9 @@
+class ClaseTraducida { 
+public static void main ( String [ ] args ) { 
+int a  =  0 ; 
+
+for ( a  =  1 ;  a  <  5 ;  a++ ) 
+System.out.println ( a ) ; 
+
+} 
+} 
